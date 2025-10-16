b482ec9a9a425e2c74e364d5539217876a7086e0
pasha-udin <pavel397@gmail.com>
2025-10-16 19:14:10 +0300

    добавил чекпоинты

--- main.py
+++ main.py
@@ -2,7 +2,19 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+things_to_do = []
+
 
 @app.get("/")
 async def root():
     return {"УБЕЙТЕ": "МЕНЯ"}
+
+
+@app.post("/add/{name}")
+async def add(name):
+    things_to_do.append(name)
+
+
+@app.get("/items")
+async def items():
+    return things_to_do
