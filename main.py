69caa4474dc19cc62a273acc1b7b0304483a4003
pasha-udin <pavel397@gmail.com>
2025-11-08 20:05:21 +0300

    sssr

--- main.py
+++ main.py
@@ -1,8 +1,44 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from contextlib import asynccontextmanager
+from sqlalchemy import create_engine, Column, Integer, String, MetaData, Table, Boolean
+from databases import Database
+from sqlalchemy import text
 
-app = FastAPI()
+DATABASE_URL = "sqlite:///./test.db"
+database = Database(DATABASE_URL)
+metadata = MetaData()
 
-things_to_do = []
+# Определение схемы таблицы
+todos = Table(
+    "todos",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("title", String(50)),
+    Column("description", String(200)),
+    Column("completed", Boolean)
+)
+
+engine = create_engine(DATABASE_URL)
+metadata.create_all(engine)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await database.connect()
+    yield
+    await database.disconnect()
+
+
+class To_do(BaseModel):
+    id: int
+    name: str
+    done: bool
+
+
+app = FastAPI(lifespan=lifespan)
+
+ggvp = {}
 
 
 @app.get("/")
@@ -10,11 +46,14 @@ async def root():
     return {"УБЕЙТЕ": "МЕНЯ"}
 
 
-@app.post("/add/{name}")
-async def add(name):
-    things_to_do.append(name)
+@app.post("/add")
+async def add(to_do: To_do):
+    if to_do.id in ggvp:
+        raise HTTPException(status_code=400, detail="Todo with this ID already exists")
+    else:
+        ggvp[to_do.id] = to_do
 
 
 @app.get("/items")
 async def items():
-    return things_to_do
+    return ggvp
